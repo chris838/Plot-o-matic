7d7e31b9d6fc2f88ce5bcf9ae62417a974b77382
Chris Harding <chris@motrr.com>
2012-09-20 20:57:04 -0700

    Simple TCP drive, can recieve data

---
+++ plugins/io_drivers/tcp.py
@@ -0,0 +1,80 @@
+from enthought.traits.api import Str, Float, Range, Int, Bool, on_trait_change
+from enthought.traits.ui.api import View, Item
+
+import time
+import socket
+
+from io_driver import IODriver
+
+class TCPDriver(IODriver):
+  """
+      TCP input driver.
+  """
+  
+  name = Str('TCP Driver')
+  view = View(
+    Item(name='port', label='Port'),
+    Item(name='filter_by_addr', label='Filter by remote address'),
+    Item(name='filter_addr', label='Filter address'),    
+    Item(name='ip', label='IP address to bind'),
+    Item(label='(use 0.0.0.0 to bind \nto all interfaces)'),
+    Item(name='show_debug_msgs', label='Show debug messages'),
+    Item(name='buffer_size', label='Buffer size / kb'),
+    Item(name='timeout', label='Timeout / s'),
+    title='TCP input driver'
+  )
+
+  
+  _sock = socket.socket()
+
+  port = Range(1024, 65535, 5000)
+  buffer_size = Range(1, 4096, 10) # no reason not to go above 4MB but there should be some limit.
+  timeout = Float(10.0)
+  ip = Str('0.0.0.0')
+  filter_by_addr = Bool(False)
+  show_debug_msgs = Bool(False)
+  filter_addr = Str('')
+
+  def open(self):
+    print "Opening"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(self.timeout) # seconds
+    s.setblocking(True)
+    s.bind((self.ip, self.port))
+    print "Listening..."
+    s.listen(1)
+    self._sock, (addr, blank) = s.accept()
+    print addr, " connected!"
+
+  def close(self):
+    print "Closing"
+    self._sock.close()
+  
+  def receive(self):
+    print "Received some data"
+    try:
+        data = self._sock.recv(1024*self.buffer_size)
+        if self.show_debug_msgs:
+            print "TCP driver: packet from '%s' size %u bytes" % (addr, len(data))
+            if self.filter_by_addr and addr != self.filter_addr:
+                return None
+    except socket.timeout:
+        return None
+    return data
+
+  def rebind_socket(self):
+    self.close()
+    self.open()
+
+  @on_trait_change('port')
+  def change_port(self):
+    self.rebind_socket()
+    
+  @on_trait_change('address')
+  def change_address(self):
+    self.rebind_socket()
+
+  @on_trait_change('timeout')
+  def change_timeout(self):
+    self.rebind_socket()
+    
